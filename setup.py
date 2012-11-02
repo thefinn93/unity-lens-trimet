e37dbc29c61eb97d5c5cc1efa595a43f09e17eac
Benjamin Kerensa <bkerensa@nova.(none)>
2012-11-02 02:36:36 -0700

    Testing Release

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+from distutils.core import setup
+from DistUtilsExtra.command import *
+
+setup(name="unity-scope-cities",
+      version="0.1",
+      author="Benjamin Kerensa",
+      author_email="bkerensa@ubuntu.com",
+      url="http://launchpad.net/~bkerensa",
+      license="GNU General Public License (GPL)",
+      data_files=[
+    ('lib/unity-scope-trimet', ['src/unity-scope-trimet']),
+    ('share/dbus-1/services', ['unity-scope-trimet.service']),
+    ('share/unity/lenses/utilities', ['trimet.scope']),
+    ('share/unity/lenses/utilities', ['icons/trimet-48.png']),
+    ], cmdclass={"build":  build_extra.build_extra, })
